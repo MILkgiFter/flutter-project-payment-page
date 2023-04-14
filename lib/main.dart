55f4d36a5089a01ee732e19c2ccf6556e418ead6
Kuspekov Diyar se2117 <103980773+MILkgiFter@users.noreply.github.com>
2023-04-14 13:18:46 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_braintree/flutter_braintree.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Braintree Payment Demo',
+      home: PaymentPage(),
+    );
+  }
+}
+
+class PaymentPage extends StatefulWidget {
+  @override
+  _PaymentPageState createState() => _PaymentPageState();
+}
+
+class _PaymentPageState extends State<PaymentPage> {
+  @override
+  void initState() {
+    super.initState();
+    initBraintreePayment();
+  }
+
+  Future<void> initBraintreePayment() async {
+    await FlutterBraintree.initialize('YOUR_TOKENIZATION_KEY');
+  }
+
+  Future<void> handleBraintreePayment() async {
+    var request = BraintreeDropInRequest(
+      tokenizationKey: 'YOUR_TOKENIZATION_KEY',
+      collectDeviceData: true,
+      googlePaymentRequest: BraintreeGooglePaymentRequest(
+        totalPrice: '10.00',
+        currencyCode: 'USD',
+        billingAddressRequired: false,
+      ),
+      paypalRequest: BraintreePayPalRequest(
+        amount: '10.00',
+        currencyCode: 'USD',
+      ),
+    );
+    BraintreeDropInResult result = await FlutterBraintree.dropInResult(request);
+    if (result != null) {
+      print(result.paymentMethodNonce.nonce);
+      // Отправьте nonce на сервер для обработки платежа
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Оплата'),
+      ),
+      body: Center(
+        child: FlatButton(
+          onPressed: () async {
+            await handleBraintreePayment();
+          },
+          child: Text('Оплатить'),
+        ),
+      ),
+    );
+  }
+}
