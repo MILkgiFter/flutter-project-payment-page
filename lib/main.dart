862dcb61015533c495fe3350d859c59045b9e3cb
Kuspekov Diyar se2117 <103980773+MILkgiFter@users.noreply.github.com>
2023-04-26 16:07:01 +0600

    ya daun tupoy

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,167 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_braintree/flutter_braintree.dart';
 
+void main() {
+  runApp(MyApp());
+}
+class MyAlertDialogTheme {
+  static final ThemeData theme = ThemeData(
+    dialogTheme: DialogTheme(
+      backgroundColor: Colors.white, // цвет фона
+      titleTextStyle: TextStyle(color: Colors.black), // цвет текста заголовка
+      contentTextStyle: TextStyle(color: Colors.black), // цвет текста содержимого
+    ),
 
-void main() => runApp(MyApp());
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Braintree Payment Demo',
-      home: PaymentPage(),
+      theme: MyAlertDialogTheme.theme,
+      title: 'Пример приложения',
+      home: MyHomePage(),
     );
   }
 }
 
-class PaymentPage extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
-  _PaymentPageState createState() => _PaymentPageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _PaymentPageState extends State<PaymentPage> {
-  @override
-  void initState() {
-    super.initState();
-    initBraintreePayment();
-  }
+class _MyHomePageState extends State<MyHomePage> {
+  bool _isDialogVisible = false;
 
-  Future<void> initBraintreePayment() async {
-    await FlutterBraintree.initialize('sandbox_8hw2fynb_5r8rv5ztbjkbvc2j');
-  }
+  void _showDialog() {
+    showDialog(
 
-  Future<void> handleBraintreePayment() async {
-    var request = BraintreeDropInRequest(
-      tokenizationKey: 'sandbox_8hw2fynb_5r8rv5ztbjkbvc2j',
-      collectDeviceData: true,
-      googlePaymentRequest: BraintreeGooglePaymentRequest(
-        totalPrice: '10.00',
-        currencyCode: 'USD',
-        billingAddressRequired: false,
-      ),
-      paypalRequest: BraintreePayPalRequest(
-        amount: '10.00',
-        currencyCode: 'USD',
-      ),
-    );
-    BraintreeDropInResult result = await FlutterBraintree.dropInResult(request);
-    if (result != null) {
-      print(result.paymentMethodNonce.nonce);
-      // Отправьте nonce на сервер для обработки платежа
-    }
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          title: Text("Страница оплаты"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  hintText: "Номер карты",
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        hintText: "MM/YY",
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        hintText: "CVV",
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+
+
+          actions: [
+            TextButton(
+              child: Text("Отмена"),
+              onPressed: () {
+                setState(() {
+                  _isDialogVisible = false;
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text("Подтвердить"),
+              onPressed: () {
+                // Действия при подтверждении
+                setState(() {
+                  _isDialogVisible = false;
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    ).then((value) {
+      // Обработка результата
+      print("Dialog result: $value");
+    });
+
+    setState(() {
+      _isDialogVisible = true;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Оплата'),
+        title: Text("Способ оплаты"),
+        backgroundColor: Colors.lightGreen,
       ),
       body: Center(
-        child: FlatButton(
-          onPressed: () async {
-            await handleBraintreePayment();
-          },
-          child: Text('Оплатить'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            OutlinedButton (
+              child: Text("Оплата картой"),
+              onPressed: () {
+                _showDialog();
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                minimumSize: MaterialStateProperty.all<Size>(Size(300, 50)),
+                padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(5)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      side: BorderSide(color: Colors.grey),
+                    )
+                ),
+              ),
+            ),
+            if (_isDialogVisible)
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.black54,
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+          ],
         ),
       ),
     );
   }
+
 }
